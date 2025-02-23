ab44d0c70c18b8fe4f407a1f569215b4529af760
sour <153382138+radio-mistress@users.noreply.github.com>
2025-02-23 15:46:29 -0600

    dji rs4 mini nonsense

--- src/src/rx-serial/SerialSBUS.cpp
+++ src/src/rx-serial/SerialSBUS.cpp
@@ -38,24 +38,64 @@ uint32_t SerialSBUS::sendRCFrame(bool frameAvailable, bool frameMissed, uint32_t
 #else
     if (config.GetSerialProtocol() == PROTOCOL_DJI_RS_PRO)
 #endif
-    {
-        PackedRCdataOut.ch0 = fmap(channelData[0], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch1 = fmap(channelData[1], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch2 = fmap(channelData[2], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch3 = fmap(channelData[3], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch4 = fmap(channelData[5], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Record start/stop and photo
-        PackedRCdataOut.ch5 = fmap(channelData[6], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Mode
-        PackedRCdataOut.ch6 = fmap(channelData[7], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 176,  848); // Recenter and Selfie
-        PackedRCdataOut.ch7 = fmap(channelData[8], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch8 = fmap(channelData[9], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch9 = fmap(channelData[10], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch10 = fmap(channelData[11], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch11 = fmap(channelData[12], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch12 = fmap(channelData[13], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch13 = fmap(channelData[14], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch14 = fmap(channelData[15], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696);
-        PackedRCdataOut.ch15 = channelData[4] < CRSF_CHANNEL_VALUE_MID ? 352 : 1696;
-    }
+{
+    // Channel Mapping for DJI RS Protocol
+    PackedRCdataOut.ch0 = fmap(channelData[0], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Ail (Pan)
+    PackedRCdataOut.ch1 = fmap(channelData[1], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Ele (Tilt)
+    PackedRCdataOut.ch2 = fmap(channelData[2], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // THR (Focus Motor)
+    PackedRCdataOut.ch3 = fmap(channelData[3], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Rud (roll)
+    PackedRCdataOut.ch4 = fmap(channelData[5], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Record start/stop and photo
+    PackedRCdataOut.ch5 = fmap(channelData[6], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Gimbal Mode Change
+    PackedRCdataOut.ch6 = fmap(channelData[7], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 176,  848); // Recenter and Selfie
+    PackedRCdataOut.ch7 = fmap(channelData[8], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Gimbal Speed Adjustment
+    PackedRCdataOut.ch8 = fmap(channelData[9], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Zoom
+    PackedRCdataOut.ch9 = (channelData[11] > CRSF_CHANNEL_VALUE_MID) ? 1696 : 352; // Locking Mode (Binary: 352 = Unlocked, 1696 = Locked)
+    PackedRCdataOut.ch10 = fmap(channelData[11], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Unassigned (Reserved)
+    PackedRCdataOut.ch11 = fmap(channelData[12], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Unassigned (Reserved)
+    PackedRCdataOut.ch12 = fmap(channelData[13], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Unassigned (Reserved)
+    PackedRCdataOut.ch13 = fmap(channelData[14], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Unassigned (Reserved)
+    PackedRCdataOut.ch14 = fmap(channelData[15], CRSF_CHANNEL_VALUE_MIN, CRSF_CHANNEL_VALUE_MAX, 352, 1696); // Unassigned (Reserved)
+    PackedRCdataOut.ch15 = channelData[4] < CRSF_CHANNEL_VALUE_MID ? 352 : 1696; // On (Reserved)
+    // Focus Motor Control (ch2)
+    uint16_t focusMotorValue = PackedRCdataOut.ch2; // Use ch2 for focus motor control
+    uint8_t focusMotorCommand[5] = {0}; // Focus motor command packet
+    focusMotorCommand[0] = 0xAA; // Header
+    focusMotorCommand[1] = 0x12; // Focus Motor Command ID
+    focusMotorCommand[2] = (focusMotorValue >> 8) & 0xFF; // High byte of focus motor value
+    focusMotorCommand[3] = focusMotorValue & 0xFF; // Low byte of focus motor value
+    focusMotorCommand[4] = 0x00; // Footer
+    // Zoom Control (ch8)
+    uint16_t zoomValue = PackedRCdataOut.ch8; // Use ch8 for zoom control
+    uint8_t zoomCommand[5] = {0}; // Zoom command packet
+    zoomCommand[0] = 0xAA; // Header
+    zoomCommand[1] = 0x13; // Zoom Command ID
+    zoomCommand[2] = (zoomValue >> 8) & 0xFF; // High byte of zoom value
+    zoomCommand[3] = zoomValue & 0xFF; // Low byte of zoom value
+    zoomCommand[4] = 0x00; // Footer
+    // Gimbal Speed Adjustment (ch7)
+    uint16_t gimbalSpeedValue = PackedRCdataOut.ch7; // Use ch7 for gimbal speed adjustment
+    uint8_t gimbalSpeedCommand[5] = {0}; // Gimbal speed command packet
+    gimbalSpeedCommand[0] = 0xAA; // Header
+    gimbalSpeedCommand[1] = 0x14; // Gimbal Speed Command ID
+    gimbalSpeedCommand[2] = (gimbalSpeedValue >> 8) & 0xFF; // High byte of gimbal speed value
+    gimbalSpeedCommand[3] = gimbalSpeedValue & 0xFF; // Low byte of gimbal speed value
+    gimbalSpeedCommand[4] = 0x00; // Footer
+    // Locking Mode (ch10)
+    uint16_t lockingModeValue = PackedRCdataOut.ch10; // Use ch10 for locking mode
+    uint8_t lockingModeCommand[5] = {0}; // Locking mode command packet
+    lockingModeCommand[0] = 0xAA; // Header
+    lockingModeCommand[1] = 0x0E; // Command Set (Gimbal Control)
+    lockingModeCommand[2] = 0x0D; // Command ID (Operating Mode Settings)
+    lockingModeCommand[3] = (lockingModeValue == 1696) ? 0x00 : 0x01; // Locking Mode (0x00 = Lock, 0x01 = Unlock)
+    lockingModeCommand[4] = 0x00; // Footer
+    
+    // Send Custom DJI Packets
+    _outputPort->write(focusMotorCommand, sizeof(focusMotorCommand)); // Focus Motor
+    _outputPort->write(zoomCommand, sizeof(zoomCommand)); // Zoom Control
+    _outputPort->write(gimbalSpeedCommand, sizeof(gimbalSpeedCommand)); // Gimbal Speed Adjustment
+    _outputPort->write(lockingModeCommand, sizeof(lockingModeCommand)); // Locking Mode
+
+}
     else
     {
         PackedRCdataOut.ch0 = channelData[0];
